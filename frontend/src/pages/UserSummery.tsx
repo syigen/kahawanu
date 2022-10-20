e84715c225816515daea3ffb012abf11d2f34308
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-10-20 17:29:16 +0530

    added search input and calender components

--- frontend/src/pages/UserSummery.tsx
+++ frontend/src/pages/UserSummery.tsx
@@ -1,10 +1,19 @@
+import React, { useState } from 'react';
+
 import AccountBalance from "components/ContentCards/AccountBalance"
 import DetailCard from "components/ContentCards/DetailCard"
 import MonthlySummery from "components/ContentCards/MonthlySummery"
 import InvoiceForm from "components/InvoiceForm"
 import NavBar from "components/NavBar"
 
+import { InputText } from 'primereact/inputtext';
+import { Calendar } from 'primereact/calendar';
+
 const UserSummery=()=>{
+
+    const [dates2, setDates2] = useState<Date | Date[] | undefined>(undefined);
+
+
     return <NavBar>
         <div className=" px-20">
             <div className=" grid grid-cols-6 gap-x-6 gap-y-8">
@@ -16,6 +25,19 @@ const UserSummery=()=>{
                 <DetailCard title="Clients"/>
                 <DetailCard title="Purchase"/>
                 <div className=" col-span-2 row-span-3"><InvoiceForm/></div>
+
+                <div className=" col-span-4 border">
+                    <p>Recent Transactions</p>
+                    <div className=" flex justify-between items-center">
+                        <span className="p-input-icon-left ">
+                            <i className="pi pi-search" />
+                            <InputText className="rounded-full p-inputtext-sm"  value="" onChange={()=>{}} placeholder="Search" />
+                        </span>
+                        <div>
+                        <Calendar id="range" value={dates2} onChange={(e) => setDates2(e.value)} selectionMode="range" readOnlyInput  showIcon/>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
