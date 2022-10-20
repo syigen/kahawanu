63ee763dc4efd6a4c9f751806035cf7f622314b3
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-10-20 11:10:31 +0530

    AccountBalance Card created

---
+++ frontend/src/pages/AccountStatus.tsx
@@ -0,0 +1,3 @@
+const AccountStatus =()=>{}
+
+export default AccountStatus;
--- frontend/src/pages/UserSummery.tsx
+++ frontend/src/pages/UserSummery.tsx
@@ -1,9 +1,10 @@
+import AccountBalance from "components/ContentCards/AccountBalance"
 import NavBar from "components/NavBar"
 
 const UserSummery=()=>{
     return <NavBar>
         <div className=" grid grid-cols-6 gap-x-4">
-            <div className=" col-span-2">Account Banlance</div>
+            <AccountBalance/>
             <div className=" col-span-2">Paid and month</div>
             <div className=" col-span-2">Outstanding this month</div>
         </div>
