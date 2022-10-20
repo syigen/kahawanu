5e8ad4d4f16a79cc3c23ddc7fad08d1ec838418b
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-10-20 11:42:44 +0530

    AccountBalance Card created

--- frontend/src/pages/UserSummery.tsx
+++ frontend/src/pages/UserSummery.tsx
@@ -1,12 +1,15 @@
 import AccountBalance from "components/ContentCards/AccountBalance"
+import MonthlySummery from "components/ContentCards/MonthlySummery"
 import NavBar from "components/NavBar"
 
 const UserSummery=()=>{
     return <NavBar>
-        <div className=" grid grid-cols-6 gap-x-4">
-            <AccountBalance/>
-            <div className=" col-span-2">Paid and month</div>
-            <div className=" col-span-2">Outstanding this month</div>
+        <div className=" px-20">
+            <div className=" grid grid-cols-6 gap-x-6">
+                <AccountBalance/>
+                <MonthlySummery title="Paid this month"/>
+                <MonthlySummery title="Outstanding this month"/>
+            </div>
         </div>
     </NavBar>
 }
