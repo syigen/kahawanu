00229c791c38bfef17abe47df13969a7eade87ea
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-10-21 13:47:39 +0530

    add logo

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -12,9 +12,14 @@ interface IProps {
 
 const NavBar=({children}:IProps)=>{
     return <div>
-        <div className=" bg-red-300 flex justify-between p-4">
+        <div className=" bg-[#FFFFFF] flex justify-between p-4">
             <div className=" flex ">
-                <Link to="/"><div className=" px-8">LOGO</div></Link>
+                <Link to="/"><div className=" px-8 flex space-x-2">
+                <svg width="26" height="33" viewBox="0 0 26 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 22V2L5.5 5L25.5 0V21L21 22V6.5L11.5 9L17 12V33L0 22Z" fill="#F99AB1"/>
+                </svg>
+                <p className=" text-[#F99AB1]">Brand Here</p>
+                </div></Link>
                 <ul className=" grid grid-cols-4 gap-x-2">
                 <Link to="/overview">Overview</Link>
                 <Link to="/invoice">Invoices</Link>
