e57030c5e697c5aed0bb4ef6bb73ee59f43d4988
dltmthilina <dltmthilinamaduranga@gmail.com>
2022-10-21 10:31:17 +0530

    add recent transaction details

--- frontend/src/pages/UserSummery.tsx
+++ frontend/src/pages/UserSummery.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 
+
+
 import AccountBalance from "components/ContentCards/AccountBalance"
 import DetailCard from "components/ContentCards/DetailCard"
 import MonthlySummery from "components/ContentCards/MonthlySummery"
 import InvoiceForm from "components/InvoiceForm"
 import NavBar from "components/NavBar"
 
+
 import { InputText } from 'primereact/inputtext';
 import { Calendar } from 'primereact/calendar';
+import UserDetail from 'components/UserDetailCard';
 
 const UserSummery=()=>{
 
@@ -26,22 +30,36 @@ const UserSummery=()=>{
                 <DetailCard title="Purchase"/>
                 <div className=" col-span-2 row-span-3"><InvoiceForm/></div>
 
-                <div className=" col-span-4 border">
-                    <p>Recent Transactions</p>
-                    <div className=" flex justify-between items-center">
-                        <span className="p-input-icon-left ">
+                <div className=" col-span-4 bg-[#F7F7FA]">
+                    <div>Recent Transactions</div>
+                    <div className="flex justify-between">
+                        <div className="p-input-icon-left flex flex-col w-1/2">
                             <i className="pi pi-search" />
-                            <InputText className="rounded-full p-inputtext-sm"  value="" onChange={()=>{}} placeholder="Search" />
-                        </span>
+                            <InputText value="" onChange={()=>{}} placeholder="search by invoice number, date" />
+                        </div>
                         <div>
                         <Calendar id="range" value={dates2} onChange={(e) => setDates2(e.value)} selectionMode="range" readOnlyInput  showIcon/>
                         </div>
                     </div>
+                    <div className=' w-4/5 flex  justify-between mt-4'>
+                        <button className=' bg-[#151B2C] text-[#D9DFFF] text-xs py-1 px-10 rounded-md'>All</button>
+                        <button className=' bg-[#E9ECFB] text-[#151B2C] text-xs py-1 px-10 rounded-md'>Draft</button>
+                        <button className=' bg-[#E9ECFB] text-[#151B2C] text-xs py-1 px-10 rounded-md'>Pending</button>
+                        <button className=' bg-[#E9ECFB] text-[#151B2C] text-xs py-1 px-10 rounded-md'>Paid</button>
+                        <button className=' bg-[#E9ECFB] text-[#151B2C] text-xs py-1 px-10 rounded-md'>Unpaid</button>
+                    </div>
+
+                    <div className='space-y-6 mt-10'>
+                        <UserDetail/>
+                        <UserDetail/>
+                        <UserDetail/>
+                        <UserDetail/>
+                    </div>
+
                 </div>
             </div>
 
         </div>
     </NavBar>
 }
-
 export default UserSummery
